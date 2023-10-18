d6040d9441924eaaf9c0d4a141152548a1d80b9f
Katarina Ozola <148257986+katarinaOzola-qa@users.noreply.github.com>
2023-10-18 20:00:15 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,9 @@
+
+var button = document.querySelector('button');
+
+
+button.addEventListener('click', function() {
+	console.log('log: Выведи этот текст в консоль если кнопка нажмётся');
+	alert('Work hard,dream big and embrace each challenge with grace')
+})
+
